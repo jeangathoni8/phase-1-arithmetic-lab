e9125dc85700ad037a658dee6ba292c3b17191b8
jeangathoni8 <jeangathoni39@gmail.com>
2024-03-28 13:43:59 +0200

    arithmetic update

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+let num1= 31;
+let num2 = 2;
+
+let multiply= num1 * num2
+
+let random = Math.ceil(Math.random() *10);
+
+//Define the variable num3 and num4
+let num3 = 20;
+let num4 = 16;
+
+//Create the variable mod and set it to the equation
+let mod = num3 % num4;
+
+//Define max num and do whatever is needed
+let numbers = [2, 10, 17, 20];
+let max = Math.max(...numbers);
